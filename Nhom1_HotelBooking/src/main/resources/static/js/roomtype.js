1e9fa26b79d6d7fd74ddf613df3cf4b0c1bc0854
HDungx <155172535+HDungx@users.noreply.github.com>
2024-04-10 14:57:49 +0700

    10/4 - Update Pagination

--- Nhom1_HotelBooking/src/main/resources/static/js/roomtype.js
+++ Nhom1_HotelBooking/src/main/resources/static/js/roomtype.js
@@ -9,7 +9,38 @@ app.controller("roomtype-ctrl", function ($scope, $http) {
 			console.log($scope.roomtypes);
 		}).catch(error=>{
 			console.log("Error",error);
-		})
+		})	
 	}
 	$scope.initialize();
+	
+	$scope.pager={
+		page:0,
+		size:5,
+		get roomtypes(){
+			var start = this.page * this.size;
+			return $scope.roomtypes.slice(start,start + this.size);
+		},
+		get count(){
+			return Math.ceil(1.0*$scope.roomtypes.length/this.size);
+		},
+		first(){
+			this.page=0;
+		},
+		previous(){
+			this.page--;
+			if(this.page<0){
+				this.last();
+			}
+		},
+		next(){
+			this.page++;
+			if(this.page>=this.count){
+				this.first();
+			}
+		},
+		last(){
+			this.page  = this.count-1;
+		}
+		
+	}
 })
